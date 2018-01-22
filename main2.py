ad06756309121911c08f94bd028f712117ad0e67
overiq <infooveriq@gmail.com>
2018-01-22 23:56:39 +0530

    Lesson 10(10a)

--- main2.py
+++ main2.py
@@ -1,5 +1,29 @@
 from flask import Flask, request, render_template
+from flask_script import Manager, Command, Shell
+
 app = Flask(__name__)
+app.debug = True
+manager = Manager(app)
+
+class Faker(Command):
+    'A command to add fake data to the tables'
+    def run(self):
+        # add logic here
+        print("Fake data entered")
+        
+manager.add_command("faker", Faker())        
+
+@manager.command
+def foo():
+    "Just a simple command"
+    print("foo command executed")
+    
+def shell_context():
+    import os, sys
+    return dict(app=app, os=os, sys=sys)
+
+manager.add_command("shell", Shell(make_context=shell_context))
+    
 
 @app.route('/')
 def index():
@@ -14,4 +38,4 @@ def books(genre):
     return "All Books in {} category".format(genre)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    manager.run()
