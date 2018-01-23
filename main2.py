f8c3b3e65814c37415984668f8726435f13c80a9
overiq <infooveriq@gmail.com>
2018-01-23 13:17:51 +0530

    Lesson 12(12a)

--- main2.py
+++ main2.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, make_response
 from flask_script import Manager, Command, Shell
 from forms import ContactForm
 
@@ -70,6 +70,32 @@ def contact():
         return redirect(url_for('contact'))
 
     return render_template('contact.html', form=form)
+
+@app.route('/cookie/')
+def cookie():
+    if not request.cookies.get('foo'):
+        res = make_response("Setting a cookie")
+        res.set_cookie('foo', 'bar', max_age=60*60*24*365*2)
+    else:
+        res = make_response("Value of cookie foo is {}".format(request.cookies.get('foo')))
+    return res
+
+@app.route('/delete-cookie/')
+def delete_cookie():
+    res = make_response("Cookie Removed")
+    res.set_cookie('foo', 'bar', max_age=0)
+    return res
+
+@app.route('/article/', methods=['POST', 'GET'])
+def article():
+    if request.method == 'POST':
+        print(request.form)
+        res = make_response("")
+        res.set_cookie("font", request.form.get('font'), 60*60*24*15)
+        res.headers['location'] = url_for('article')
+        return res, 302
+    
+    return render_template('article.html')
 
 if __name__ == "__main__":
     manager.run()
