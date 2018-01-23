1296fafba7d1f3760181741aaf0dd1b90831bf91
overiq <infooveriq@gmail.com>
2018-01-23 15:12:41 +0530

    Lesson 14(14a)

--- main2.py
+++ main2.py
@@ -1,12 +1,16 @@
 from flask import Flask, request, render_template, redirect, url_for, flash, make_response, session
 from flask_script import Manager, Command, Shell
 from forms import ContactForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'a really really really really long secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:pass@localhost/flask_app_db'
 
 manager = Manager(app)
+db = SQLAlchemy(app)
 
 class Faker(Command):
     'A command to add fake data to the tables'
@@ -122,6 +126,46 @@ def updating_session():
         session['cart_item'] = cart_item
 
     return res
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    slug = db.Column(db.String(255), nullable=False)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+    posts = db.relationship('Post', backref='category')
+
+    def __repr__(self):
+        return "<{}:{}>".format(self.id, self.name)
+    
+post_tags = db.Table('post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('posts.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'))
+)    
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    slug = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+    updated_on = db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+    category_id = db.Column(db.Integer(), db.ForeignKey('categories.id'))    
+
+    def __repr__(self):
+        return "<{}:{}>".format(self.id, self.title[:10])
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    slug = db.Column(db.String(255), nullable=False)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+    posts = db.relationship('Post', secondary=post_tags, backref='tags')
+
+    def __repr__(self):
+        return "<{}:{}>".format(self.id, self.name)    
 
 if __name__ == "__main__":
     manager.run()
